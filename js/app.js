60b7f94fac80811a7188c56e8fac9b10c499d912
mccafee025 <ademaamanka@gmail.com>
2021-09-07 11:37:09 +0300

    initial commit

---
+++ js/app.js
@@ -0,0 +1,14 @@
+let add = function (number1, number2) {
+  return number1 + number2;
+};
+let number1 = parseInt(prompt("enter a number"));
+let number2 = parseInt(prompt("enter another number"));
+
+
+alert(add(number1, number2));
+
+
+
+
+
+
