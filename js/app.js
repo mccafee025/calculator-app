06d45dec53ea07c024a1db509289ef15ea3c60de
mccafee025 <ademaamanka@gmail.com>
2021-09-07 11:44:01 +0300

    add a variable

--- js/app.js
+++ js/app.js
@@ -4,8 +4,8 @@ let add = function (number1, number2) {
 let number1 = parseInt(prompt("enter a number"));
 let number2 = parseInt(prompt("enter another number"));
 
-
-alert(add(number1, number2));
+let result = add(number1, number2);
+alert(result);
 
 
 
